7208b3260ba1d80148d7f7bb6ebdafd59590621a
Rayyan_Nabil <rayyannabil170@gmail.com>
2025-07-13 19:05:40 +0300

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:mal3b/screens/login_screen.dart';
+import 'package:mal3b/constants/colors.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() {
   runApp(const Mal3bApp());
@@ -10,6 +11,50 @@ class Mal3bApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: LoginScreen());
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: CustomColors.primary,
+        scaffoldBackgroundColor: CustomColors.white,
+        fontFamily: 'MadaniArabic',
+        textSelectionTheme: TextSelectionThemeData(
+          cursorColor: Colors.black,
+          selectionColor: CustomColors.primary.withOpacity(0.2),
+          selectionHandleColor: CustomColors.primary,
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: CustomColors.primary,
+
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(12),
+            ),
+          ),
+        ),
+        appBarTheme: const AppBarTheme(
+          backgroundColor: CustomColors.primary,
+          foregroundColor: Colors.white,
+          elevation: 0,
+        ),
+      ),
+      locale: const Locale('ar'),
+      supportedLocales: const [Locale('en'), Locale('ar')],
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      debugShowCheckedModeBanner: false,
+      home: const Scaffold(
+        body: Center(
+          child: Text(
+            "Hello",
+            style: TextStyle(
+              fontFamily: "MadaniArabic",
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
