4defe579a3fffc36c24c7c9ba2a611296d432fe1
Rayyan_Nabil <rayyannabil170@gmail.com>
2025-07-14 19:01:37 +0300

    auth_cubit

--- lib/logic/cubit/authentication_cubit.dart
+++ lib/logic/cubit/authentication_cubit.dart
@@ -1,12 +1,63 @@
+import 'dart:developer';
+
 import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:mal3b/api/dio_client.dart';
 
 part 'authentication_state.dart';
 
 class AuthenticationCubit extends Cubit<AuthenticationState> {
   AuthenticationCubit() : super(AuthenticationInitial());
 
-  void signup(){
+  Dio dio = DioClient().dio;
+  FlutterSecureStorage storage = FlutterSecureStorage();
+
+  void signup(
+    {
+      required name,
+      required phone,
+      required password ,
+      
+    }
+
+  ) async
+  {
+    emit(AuthenticationLoading());
+    dio.options.headers = {'content-type':'application/json'};
+    try{
+      final response = await dio.post("${DioClient.baseUrl}/signup",
+      data: {
+        "name": name,
+        "phone": phone,
+        "password": password,
+        "address":"address"
+      }
+
+      );
+      if (response.statusCode == 201) {
+        final storage = FlutterSecureStorage();
+        log("Access Token: ${response.data['accessToken']}");
+        log("Refresh Token: ${response.data['refreshToken']}");
+
+        storage.write(key: "accessToken", value: response.data['accessToken']);
+        storage.write(
+          key: "refreshToken",
+          value: response.data['refreshToken'],
+        );
+        emit(AuthenticationSignUpSuccess());
+      } else {
+        emit(AuthenticationSignUpError(msg: response.data['message']));
+      }
+    } on DioException catch (e) {
+      if (e.response != null) {
+        emit(AuthenticationSignUpError(msg: e.response!.data['message']));
+      } else {
+        emit(AuthenticationSignUpError(msg: "حدث خطأ غير متوقع"));
+      }
+    } catch (e) {
+      emit(AuthenticationSignUpError(msg: 'حدث خطأ غير متوقع'));
+    }
     
   }
    
